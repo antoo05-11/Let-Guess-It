ac153f31c9b8be3eb69db824a2a91d0aa2e3a37d
antoo05_11 <antovip123@gmail.com>
2022-02-26 08:31:15 +0700

    add file

---
+++ phrasesUsed.h
@@ -0,0 +1,7 @@
+#include <string>
+#define MAX 100
+#define MIN 1
+std::string congratulationQuote = "Congratulations!!!";
+std::string getYesOrNoQuote = "Do you want to play the game?";
+std::string getAnsQuote = "Enter your number = ";
+std::string printResQuote = "Your point is ";
